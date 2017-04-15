28347a0d56a287bb6f5025ed8d0ffd9c85ae7df0
ezdiy <ezzzdiy@gmail.com>
2017-04-15 17:17:59 +0200

    refactor, error handling

--- ssb-mnemonic.js
+++ ssb-mnemonic.js
@@ -1,4 +1,4 @@
-var M = require('./ed25519-mnemonic')
+var mnemonic = require('./ed25519-mnemonic')
 try { var P = require('proquint') } catch(e) {}
 var fs = require('fs')
 var chloride = require('chloride')
@@ -7,7 +7,7 @@ function home() {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
-var dict = new M.Dict(fs.readFileSync("wordlist8k.txt").toString().split("\n").filter(function(x) { return x.length > 0 }))
+var dict = new mnemonic.Dict(fs.readFileSync("wordlist8k.txt").toString().split("\n").filter(function(x) { return x.length > 0 }))
 
 if (!process.argv[2]) {
   var priv = new Buffer(JSON.parse(fs.readFileSync(home() + '/.ssb/secret').toString().replace(/^#.*/mg,'')).private, 'base64')
@@ -17,20 +17,20 @@ if (!process.argv[2]) {
     console.log(P.encode(priv.slice(0,32)))
   }
   console.log("\nThe mnemonic is: ")
-  console.log(M.private_to_mnemonic(priv, dict).join(' '))
+  console.log(mnemonic.private_to_mnemonic(priv, dict).join(' '))
 } else {
   var seed, phrase = process.argv[2]
   phrase = phrase.split(' ')
-  if (phrase.length === 1 && P) {
+  if (phrase.length === 1) {
+    if (phrase[0].length !== 95)
+      throw new Error("your chant is wrong")
     seed = P.decode(phrase[0])
   } else {
-    seed = M.mnemonic_to_seed(phrase, dict)
-    if (!seed) {
-      console.log('incorrect mnemonic')
-      return
-    }
+    seed = mnemonic.mnemonic_to_seed(phrase, dict)
+    if (!seed)
+      throw new Error('incorrect mnemonic')
   }
-  console.log("Reconstructed key from supplied mnemonic: ")
+  console.log("Reconstructed key from supplied chant/mnemonic: ")
   var res = chloride.crypto_sign_seed_keypair(new Buffer(seed))
   var pk = res.publicKey.toString('base64')
   var sk = res.secretKey.toString('base64')
